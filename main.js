41a1a20bd941d34d9bc4af8a161bd55fdc962748
Your Name <you@example.com>
2024-04-18 13:14:19 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+let numburOne = prompt("String taypini  kiriting")
+let text1 = prompt("Number taypini  kiriting")
+let text2 = prompt("Boolean taypini  kiriting")
+
+alert("Hamma malumotni to'g'ri kiritganizga ishonchiz komilmi? ")
+
+alert("numburOne = " + numburOne +" /text1 = " + text1 + " /text2 =" +text2)
